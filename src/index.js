214934b9b047673aeba81eab921432b3b3d49ec5
Edward Angeles <edward.angelesm@gmail.com>
2020-04-06 22:04:22 +0100

    added router to better break out code. added delete request for user and task.

--- src/index.js
+++ src/index.js
@@ -1,77 +1,14 @@
 const express = require('express')
 require('./db/mongoose.js')
-
-const User = require('./models/user.js')
-const Task = require('./models/task.js')
+const userRouter = require('./routers/user.js')
+const taskRouter = require('./routers/task.js')
 
 const app = express()
 const port = process.env.PORT || 3000 // <- for heroku
 
 app.use(express.json()) // <- automatically parse incoming json into objects
-
-app.post('/users', async (req, res) => {
-  const user = new User(req.body)
-  
-  try {
-    await user.save()
-    res.status(201).send(user)
-  } catch (error) {
-    res.status(400).send(error)
-  }
-})
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
-
-app.get('/users/:id', async (req, res) => {
-  const _id =  req.params.id
-  
-  try {
-    const user = await User.findById(_id)
-    if (!user) return res.status(404).send()
-    res.send(user)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
-
-app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body)
-  
-  try {
-    await task.save()
-    res.status(201).send(task)
-  } catch (error) {
-    res.status(400).send(error)
-  }
-})
-
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({})
-    res.send(tasks)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-  const _id = req.params.id
-  
-  try {
-    const task = await Task.findById(_id)
-    if (!task) return res.status(404).send()
-    res.send(task)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
